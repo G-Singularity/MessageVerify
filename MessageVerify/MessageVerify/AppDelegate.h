19291112bc9498db8f8a61118b865370c674671f
ArsenalMyth <amyth263@163.com>
2016-07-02 13:43:34 +0800

    0. 项目初始化

---
+++ MessageVerify/MessageVerify/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MessageVerify
+//
+//  Created by myth on 2/7/16.
+//  Copyright © 2016 myth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MessageVerify/MessageVerify/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MessageVerify
+//
+//  Created by myth on 2/7/16.
+//  Copyright © 2016 myth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
